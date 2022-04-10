a90c22a0486f6b723a661646f3556cdcd6f434bd
anthonycastellano <=>
2022-04-09 21:33:39 -0400

    page layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import AllSchedules from './pages/AllSchedules';
+import Admin from './pages/Admin';
+
+// components
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        test
-      </header>
+    <div className='App'>
+      <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        <Route exact path='/all-schedules'>
+          <AllSchedules />
+        </Route>
+
+        <Route exact path='/admin'>
+          <Admin />
+        </Route>
+
+        <Route path='*'>
+          <Redirect to='/' />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ frontend/src/pages/AllSchedules.js
@@ -0,0 +1,6 @@
+
+const AllSchedules = () => {
+    return <h1>All schedules</h1>;
+};
+
+export default AllSchedules;
