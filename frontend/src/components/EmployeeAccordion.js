17bd1b7c856abea2554650ff9f04d5914783cc9b
Anthony Castellano <tonycastellano13@gmail.com>
2022-05-28 19:25:13 -0400

    add root styling, employee list updates

--- frontend/src/components/EmployeeAccordion.js
+++ frontend/src/components/EmployeeAccordion.js
@@ -4,15 +4,25 @@ import EmployeeAccordionItem from './EmployeeAccordionItem';
 // css
 import classes from './css/Accordion.module.css';
 
-const EmployeeAccordion = ({
-    employees
-}) => {
+const EmployeeAccordion = ({employees}) => {
+    const [activeIndex, setActiveIndex] = useState();
+
     const renderedEmployees = employees.map(((employee, index) => {
-        
+        const showDescription = index === activeIndex;
+
+        return (
+            <EmployeeAccordionItem 
+                employee={employee}
+                index={index}
+                showDescription={showDescription}
+                onClick={() => { setActiveIndex(index) }}
+                key={`${employee._id}-item`}
+            />
+        );
     }));
 
     return (
-        <div>
+        <div className={classes.employees}>
             {renderedEmployees}
         </div>
     )
